c2d533edfcfcce72ad66884cfa1e066cf34f2590
denis.pujdak <denis.pujdak@hotmail.com>
2022-06-17 17:26:58 +0100

    https://github.com/dotnet/maui/issues/7965#issuecomment-1159037586

---
+++ MauiApp1/wwwroot/MauiApp1.lib.module.js
@@ -0,0 +1,9 @@
+﻿export function beforeStart(options, extensions) {
+    console.log("beforeStart");
+}
+
+export function afterStarted(blazor) {
+    var jq = document.createElement('script');
+    jq.src = "/index.js";
+    document.getElementsByTagName('head')[0].appendChild(jq);
+}
---
+++ MauiApp1/wwwroot/index.js
@@ -0,0 +1,11 @@
+﻿function CallCSharp() {
+    try {
+        window.DotNet.invokeMethodAsync('MauiApp1', 'CallMeFromJs', "Hello World").then(result => {
+            document.getElementById("app").innerHTML = result;
+        });
+    } catch (error) {
+        document.getElementById("app").innerHTML = error;
+    }
+}
+
+CallCSharp();
