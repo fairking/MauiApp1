441e33e7d81848b84593184f4f99583d310d34b7
fairking <fairking@mooo.com>
2022-06-18 20:56:10 +0100

    Better but not the best approach

--- MauiApp1/wwwroot/MauiApp1.lib.module.js
+++
@@ -1,9 +0,0 @@
-﻿export function beforeStart(options, extensions) {
-    console.log("beforeStart");
-}
-
-export function afterStarted(blazor) {
-    var jq = document.createElement('script');
-    jq.src = "/index.js";
-    document.getElementsByTagName('head')[0].appendChild(jq);
-}
